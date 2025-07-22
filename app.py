df95041d27ea5bc5366f3bb12d430552dd36ed6e
Zi Xin <chenzixin1@gmail.com>
2025-07-22 22:23:20 +0800

    feat: add API_KEY env support and dotenv integration for CD

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import yt_dlp
 import os
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
